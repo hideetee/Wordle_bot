7ca03d4fff1b779c4f529c6c7ee6576a85f0c181
Haidee Tang <Haidee.Tang95@gmail.com>
2026-07-24 14:07:10 +0100

    Updated scorer function to return a dataframe of rankings and raw scores for each week

--- src/wordle_bot/scorer.py
+++ src/wordle_bot/scorer.py
@@ -1,7 +1,8 @@
-
 # ==============================
 # SCORING
 # ==============================
+
+import polars as pl
 
 class ScoreCalculator:
     
@@ -14,6 +15,41 @@ class ScoreCalculator:
 
     def __repr__(self):
         return f"(player={self.player}, wordle={self.wordle_num}, score={self.score})\n"
+
+    def score_cleaner(self):
+        """
+        Create a polars DataFrame from a list of ScoreCalculator objects
+        Convert 'X' to 7 and cast score to Int64 for ranking purposes
+        If wordle_num is not complete for a player, populate score with 7
+        """
+
+        
+        data = [(score.player, score.wordle_num, score.score) for score in self]
+        df = pl.DataFrame(data, schema=["player", "wordle_num", "score"], orient = "row")
+        df = df.with_columns(
+            pl.when(pl.col("score") == "X")
+            .then(7)
+            .otherwise(pl.col("score"))
+            .alias("score").cast(pl.Int64)
+        )
+
+        players = df.select(pl.col("player")).unique()
+        wordles = df.select(pl.col("wordle_num")).unique()
+
+        full_grid = players.join(wordles, how="cross")
+
+        df_fill_incompletes = full_grid.join(
+            df, on=["player", "wordle_num"], how="left"
+            )
+
+
+        df_fill_incompletes = df_fill_incompletes.with_columns(
+            pl.col("score").fill_null(7)
+        )
+
+        df_fill_incompletes = df_fill_incompletes.sort(["wordle_num", "player"])
+
+        return df_fill_incompletes
         
 
     def numeric_score(self):
@@ -49,39 +85,78 @@ class ScoreCalculator:
 
 
     @staticmethod
-    def store_week_scores(scores, wordle_num):
+    def store_week_ranges(df):
         """
-        Store the scores for a specific Wordle week.
+        Store the week_ranges for all unique Wordle weeks in a DataFrame that is .
         """
 
-        scores = ScoreCalculator.numeric_score(scores)  
-        week_start, week_end = ScoreCalculator.wordle_week(wordle_num)
-        
-        return [s for s in scores if week_start <= s.wordle_num <= week_end]
+        week_ranges = []
+
+        week_start, week_end = ScoreCalculator.wordle_week(df['wordle_num'].unique()[0])
+
+        week_ranges.append((week_start, week_end))
+
+        wordle_numbers = df['wordle_num'].unique()
+
+        for w_num in wordle_numbers[1:]:
+            if w_num > week_end:
+                week_start, week_end = ScoreCalculator.wordle_week(w_num)
+                week_ranges.append((week_start, week_end))
+
+        return week_ranges
         
 
     @staticmethod
-    def weekly_score(scores, wordle_num):
+    def compute_weekly_score(df):
         """
         Calculate the total score for a player within a specific Wordle week.
         """
-        week_total_score = 0
 
-        week_start, week_end = ScoreCalculator.wordle_week(wordle_num)
-
-        for score in scores:
-            if week_start <= score.wordle_num <= week_end:
-                week_total_score += score
-        return week_total_score
-
-
+        weekly_dfs = []
+        for week_start, week_end in ScoreCalculator.store_week_ranges(df):
+           df_week = df.filter(
+               (pl.col("wordle_num") >= week_start) 
+               & (pl.col("wordle_num") <= week_end)
+           )
+           weekly_dfs.append(df_week)
 
 
-    def ranking(self):
+        weekly_scores = []
+
+        for df_week in weekly_dfs:
+            weekly_score = df_week.group_by('player').agg(pl.sum('score')).sort('score')
+            weekly_scores.append(weekly_score)
+
+        return weekly_scores
+
+
+
+
+    def ranking(df):
 
         '''
         Rank players based on 'wordle' and 'score'
         '''
+
+        weekly_scores = ScoreCalculator.compute_weekly_score(df)
+
+        for weekly_score in weekly_scores:
+
+            # competition ranking with mean ranks
+            df_ranked  = weekly_score.with_columns(
+                pl.arange(1, weekly_score.height +1).alias("raw_rank")
+            )
+
+            # compute mean ranks per score group
+            df_ranked_grouped = df_ranked.group_by("score").agg(
+                pl.col("raw_rank").mean().alias("rank")
+            )
+
+            df_final = df_ranked.join(df_ranked_grouped, on="score", how="left").sort("rank")
+            df_final = df_final.drop("raw_rank")
+
+            return df_final
+
         
         
 
